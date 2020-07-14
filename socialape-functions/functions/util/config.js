c8b3daed375f4b20f62e24b6cd70ce1e3242459b
vince.pacarat@awsys-i.com <vince.pacarat@awsys-i.com>
2020-07-14 15:34:59 +0800

    Exercise 1

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCqHAJ7tg78v4BaWvxULt6NULsOjSGc5Fg",
+  authDomain: "socialapes-4fefc.firebaseapp.com",
+  databaseURL: "https://socialapes-4fefc.firebaseio.com",
+  projectId: "socialapes-4fefc",
+  storageBucket: "socialapes-4fefc.appspot.com",
+  messagingSenderId: "865079623941",
+  appId: "1:865079623941:web:f21bac44eb0322b1b8376c",
+  measurementId: "G-11YEG2DH1W",
+};
